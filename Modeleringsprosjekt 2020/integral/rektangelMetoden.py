c5481bf3fd4aaa4fb0b4925a4295c233894adcf7
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2020-03-20 11:32:39 +0100

    Added different rules for rektangelmetoden

--- Modeleringsprosjekt 2020/integral/rektangelMetoden.py
+++ Modeleringsprosjekt 2020/integral/rektangelMetoden.py
@@ -2,11 +2,22 @@
 # En impementering av rektangelmetoden for å integrere
 #
 
-def RektangelMetoden(f, a, b, N = 100000):
+def RektangelMetoden(f, a, b, N = 100000, regel = "venstre"):
     # f : En funksjon å integrere
     # a : Starten på det bestemte integralet
     # b : Slutten på det bestemte integralet
     # N : Antall rektangler
+    # regel : Hvor man skal regne ut høyden ["venstre" | "midten" | "hoyre"]
+
+    # Regne ut hva man må legge til i for å regne høyden på riktig sted
+    i_regel = 0
+    if regel.lower() == "midten":
+        i_regel = 0.5
+    elif regel.lower() == "hoyre":
+        i_regel = 1
+    elif regel.lower() != "venstre":
+        # Kast en feil melding hvis regel argumentet er brukt feil
+        raise Exception(f"Regel må være [\"venstre\" (Standard) | \"midten\" | \"hoyre\"], men det ble spesifisert {regel}")
 
     # Bredden per rektangel
     dx = (b - a) / N
@@ -16,7 +27,7 @@ def RektangelMetoden(f, a, b, N = 100000):
     # Loop gjennom alle rekangelene
     for i in range(N):
         # Legg sammen høyden i rektanglene
-        A += f(a + i * dx)
+        A += f(a + (i + i_regel) * dx)
 
     # Multipliser med bredden per integral for å finne arealet
     A *= dx
