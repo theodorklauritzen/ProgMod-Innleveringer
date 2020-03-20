8e180cf6c4582a91817f0c3618d4d0c6709ab69f
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2020-03-20 17:09:39 +0100

    Shortened the code a bit

--- Modeleringsprosjekt 2020/integral/trapesMetoden.py
+++ Modeleringsprosjekt 2020/integral/trapesMetoden.py
@@ -8,17 +8,16 @@ def TrapesMetoden(f, a, b, N = 100000):
     # b : Slutten på det bestemte integralet
     # N : Antall trapeser
 
-    # Bredden per rektangel
+    # Bredden per trapes
     dx = (b - a) / N
     # Arealet under grafen
-    A = f(a) / 2
+    A = (f(a) + f(b)) / 2
 
     # Loop gjennom alle trapesene
     for i in range(1, N):
         # Legg sammen høyden i rektanglene
         A += f(a + i * dx)
 
-    A += f(b) / 2
     # Multipliser med bredden
     A *= dx
 
